64e72b14646e36db5d651506dc198c8a0257e087
Boris Veselov <borisveselov@yahoo.com>
2022-08-19 14:24:28 -0400

    portfolio set up

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.scss'
 export default function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
-     
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+        <li>Project 4</li>
+        <li>Project 5</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src='' alt=''/>
+          <h3>Web Dev</h3>
+        </div>
+      </div>
     </div>
   )
 }

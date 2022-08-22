eaede12ebec90e02b7589d8a2f140d72e318d10e
Boris Veselov <borisveselov@yahoo.com>
2022-08-22 13:22:56 -0400

    set up Works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,9 @@ import './works.scss'
 export default function Works() {
   return (
     <div className='works' id='works'>
-      
+      <div className='slider'>
+        <div className=''
+      </div>
     </div>
   )
 }

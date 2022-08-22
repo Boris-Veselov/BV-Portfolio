0a8025abf54f04b6d5ce007fcfd3fa389a9c61f5
Boris Veselov <borisveselov@yahoo.com>
2022-08-22 13:13:52 -0400

    finished portoflio list

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,16 @@
-import PortfolioList from '../portfoliolist/PortfolioList';
+import PortfolioList from '../portfolioList/PortfolioList';
 import { useEffect, useState } from "react";
 import './portfolio.scss';
+import { featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio} from '../../data';
 
 export default function Portfolio() {
-  // const [selected, setSelected] = useState("featured");
+  const [selected, setSelected] = useState('featured');
+  const [data, setData] = useState([]);
+
 
 // {/* <ul>
 //         <li>web</li>
@@ -21,10 +28,6 @@ export default function Portfolio() {
       title: 'Web',
     },
     {
-      id: 'project 3',
-      title: 'project 3',
-    },
-    {
       id: 'mobile',
       title: 'Mobile App',
     },
@@ -37,29 +40,51 @@ export default function Portfolio() {
       title: 'Content',
     },
   ];
+
+  useEffect(() =>{
+
+    switch(selected){
+      case 'featured':
+        setData(featuredPortfolio);
+        break;
+        case 'web':
+        setData(webPortfolio);
+        break;
+        case 'mobile':
+        setData(mobilePortfolio);
+        break;
+        case 'design':
+        setData(designPortfolio);
+        break;
+        case 'content':
+        setData(contentPortfolio);
+        break;
+        default:
+          setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
         {list.map((item) => (
-          <PortfolioList title={item.title}
-            // active={selected === item.id}
-            // setSelected={setSelected}
-            // id={item.id}
+          <PortfolioList 
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
           />
         ))}
       </ul>
       <div className='container'>
-        <div className='item'>
-          <img src='' alt=''/>
-          <h3>Web Dev</h3>
-        </div>
-        <div className='item'>
-          <img src='' alt=''/>
-          <h3>Web Dev</h3>
-        </div>
-        
+        {data.map((d) => (
+          <div className='item'>
+            <img src={d.img} alt=''/>
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }

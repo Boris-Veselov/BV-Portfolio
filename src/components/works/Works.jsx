3bb91eead33d2d75c56551f254863047f49b9fd9
Boris Veselov <borisveselov@yahoo.com>
2022-08-22 13:46:09 -0400

    works styles

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,15 +1,26 @@
 import './works.scss'
 
 export default function Works() {
-  return 
-    <div className='works' id='works'>
+  return <div className='works' id='works'>
       <div className='slider'>
-        <div className='conteiner'>
+        <div className='container'>
           <div className='item'>
             <div className='left'></div>
+              <div className='leftContainer'>
+                <div className='imgContainer'>
+                  <img src='assets/mobile.png' alt=''/>
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing 
+                  elit. Non rem totam aliquid praesentium accusantium 
+                  cumque nam neque incidunt sed illum tempora sequi. 
+                </p>
+                <span>Projects</span>
+              </div>
               <div className='right'></div>
           </div>
         </div>
       </div>
-    </div>
+    </div>;
 }

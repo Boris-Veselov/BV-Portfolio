697d3d4a6cc2c3190290cc03fa1f35c574e048ee
Boris Veselov <borisveselov@yahoo.com>
2022-08-22 14:19:48 -0400

    works arrow layout

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,11 +1,12 @@
 import './works.scss'
 
 export default function Works() {
-  return <div className='works' id='works'>
+  return (
+    <div className='works' id='works'>
       <div className='slider'>
         <div className='container'>
           <div className='item'>
-            <div className='left'></div>
+            <div className='left'>
               <div className='leftContainer'>
                 <div className='imgContainer'>
                   <img src='assets/mobile.png' alt=''/>
@@ -14,13 +15,18 @@ export default function Works() {
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing 
                   elit. Non rem totam aliquid praesentium accusantium 
-                  cumque nam neque incidunt sed illum tempora sequi. 
                 </p>
                 <span>Projects</span>
               </div>
-              <div className='right'></div>
+              <div className='right'>
+                <img src="https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900" alt=''/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>;
+      <img src='assets/arrow.png' className='arrow left' alt=''/>
+      <img src='assets/arrow.png' className='arrow right' alt=''/>
+    </div>
+  );
 }

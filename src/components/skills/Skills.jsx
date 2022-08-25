dc1a67c10f39b8d8b525932eb972ba376d202788
Boris Veselov <borisveselov@yahoo.com>
2022-08-24 22:58:47 -0400

    skills setup

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,8 +1,20 @@
 import './skills.scss'
+
 export default function Skills() {
   return (
     <div className='skills' id='skills'>
-      
+        <h1>Skills</h1>
+        <div className='container'>
+          <div className='card'>
+            <div className="top">
+              <img src="assest/right-arrow.png" className='left' alt="" />
+              <img src='https://images.pexels.com/photos/428321' alt=''/>
+              <img src='assests/youtube.png' alt=''/>
+            </div>
+            <div className="center"></div>
+            <div className="bottom"></div>
+          </div>
+        </div>
     </div>
   )
 }

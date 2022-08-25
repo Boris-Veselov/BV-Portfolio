2604066d3a61bf45960b60fc390a1c97d51e6541
Boris Veselov <borisveselov@yahoo.com>
2022-08-24 22:25:13 -0400

    works update layout

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,21 +1,50 @@
 import './works.scss'
 
 export default function Works() {
+
+  const data = [
+    {
+      id: "1",
+        icon: "./assets/mobile.png",
+        title: "Web Design",
+        desc:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        img:
+          "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+      },
+      {
+        id: "2",
+        icon: "./assets/globe.png",
+        title: "Mobile Application",
+        desc:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        img:
+          "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+      },
+      {
+        id: "3",
+        icon: "./assets/writing.png",
+        title: "Branding",
+        desc:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        img:
+          "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
   return (
     <div className='works' id='works'>
       <div className='slider'>
+        {data.map((d) => (
         <div className='container'>
           <div className='item'>
             <div className='left'>
               <div className='leftContainer'>
                 <div className='imgContainer'>
-                  <img src='assets/mobile.png' alt=''/>
+                  <img src={d.icon} alt=''/>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing 
-                  elit. Non rem totam aliquid praesentium accusantium 
-                </p>
+                <h2>{d.title}</h2>
+                <p>{d.desc}</p>
                 <span>Projects</span>
               </div>
               <div className='right'>
@@ -24,6 +53,7 @@ export default function Works() {
             </div>
           </div>
         </div>
+        ))}
       </div>
       <img src='assets/arrow.png' className='arrow left' alt=''/>
       <img src='assets/arrow.png' className='arrow right' alt=''/>

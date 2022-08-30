ac26ccce801776c0e7522da9823304dc0223f562
Boris Veselov <borisveselov@yahoo.com>
2022-08-30 11:55:33 -0400

    updating style

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,90 +1,90 @@
-import PortfolioList from '../portfolioList/PortfolioList';
-import { useEffect, useState } from "react";
+// import PortfolioList from '../portfolioList/PortfolioList';
+// import { useEffect, useState } from "react";
+// import './portfolio.scss';
+// import { fullstackPortfolio,
+//   frontendPortfolio,
+//   backendPortfolio,} from '../../data';
+
+// export default function Portfolio() {
+//   const [selected, setSelected] = useState('full stack');
+//   const [data, setData] = useState([]);
+
+//   const list = [
+//     {
+//       id: 'full stack',
+//       title: 'full stack',
+//     },
+//     {
+//       id: 'front-end',
+//       title: 'front-end',
+//     },
+//     {
+//       id: 'back-end',
+//       title: 'back-end',
+//     },
+//   ];
+
+//   useEffect(() =>{
+
+//     switch(selected){
+//       case 'full stack':
+//         setData(fullstackPortfolio);
+//         break;
+//         case 'front-end':
+//         setData(frontendPortfolio);
+//         break;
+//         case 'back-end':
+//         setData(backendPortfolio);
+//         break;
+//         default:
+//           setData(fullstackPortfolio);
+//     }
+//   }, [selected]);
+
+//   return (
+//     <div className="portfolio" id="portfolio">
+//       <h1>Portfolio</h1>
+//       <ul>
+//         {list.map((item) => (
+//           <PortfolioList 
+//             title={item.title}
+//             active={selected === item.id}
+//             setSelected={setSelected}
+//             id={item.id}
+//           />
+//         ))}
+//       </ul>
+//       <div className='container'>
+//         {data.map((d) => (
+//           <div className='item'>
+//             <img src={d.img} alt=''/>
+//             <h3>{d.title}</h3>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from 'react'
 import './portfolio.scss';
-import { featuredPortfolio,
-  webPortfolio,
-  mobilePortfolio,
-  designPortfolio,
-  contentPortfolio} from '../../data';
+
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState('featured');
-  const [data, setData] = useState([]);
-
-
-// {/* <ul>
-//         <li>web</li>
-//         <li>print</li>
-//         </ul>
-//       .container */}
-
-  const list = [
-    {
-      id: 'featured',
-      title: 'Featured',
-    },
-    {
-      id: 'web',
-      title: 'Web',
-    },
-    {
-      id: 'mobile',
-      title: 'Mobile App',
-    },
-    {
-      id: 'design',
-      title: 'Design',
-    },
-    {
-      id: 'content',
-      title: 'Content',
-    },
-  ];
-
-  useEffect(() =>{
-
-    switch(selected){
-      case 'featured':
-        setData(featuredPortfolio);
-        break;
-        case 'web':
-        setData(webPortfolio);
-        break;
-        case 'mobile':
-        setData(mobilePortfolio);
-        break;
-        case 'design':
-        setData(designPortfolio);
-        break;
-        case 'content':
-        setData(contentPortfolio);
-        break;
-        default:
-          setData(featuredPortfolio);
-    }
-  }, [selected]);
-
   return (
-    <div className="portfolio" id="portfolio">
+    <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>
       <ul>
-        {list.map((item) => (
-          <PortfolioList 
-            title={item.title}
-            active={selected === item.id}
-            setSelected={setSelected}
-            id={item.id}
-          />
-        ))}
+        <li className='active'>full stack</li>
+        <li>front-end</li>
+        <li>back-end</li>
       </ul>
       <div className='container'>
-        {data.map((d) => (
-          <div className='item'>
-            <img src={d.img} alt=''/>
-            <h3>{d.title}</h3>
-          </div>
-        ))}
+        <div className='item'>
+          <img src='assets/wds.png' alt=''/>
+          <h3>web dev social</h3>
+        </div>
       </div>
     </div>
-  );
+  )
 }
--- src/data.js
+++ src/data.js
@@ -1,15 +1,14 @@
-export const featuredPortfolio = [
+export const fullstackPortfolio = [
     {
       id: 1,
-      title: "Social Media App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+      title: "web dev social",
+      
     },
     {
       id: 2,
       title: "Rampa UI Design",
       img:
-        "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+        'wds.png',
     },
     {
       id: 3,
@@ -37,7 +36,7 @@ export const featuredPortfolio = [
     },
   ];
   
-  export const webPortfolio = [
+  export const frontendPortfolio = [
     {
       id: 1,
       title: "Web Social Media App",
@@ -70,7 +69,7 @@ export const featuredPortfolio = [
     },
   ];
   
-  export const mobilePortfolio = [
+  export const backendPortfolio = [
     {
       id: 1,
       title: "Mobile Social Media App",
@@ -106,83 +105,5 @@ export const featuredPortfolio = [
       title: "Mobile Banking App",
       img:
         "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
-    },
-  ];
-  
-  export const designPortfolio = [
-    {
-      id: 1,
-      title: "Design Social Media App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 2,
-      title: "Design Rampa UI Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 3,
-      title: "Design E-commerce Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 4,
-      title: "Design Relax Mobile App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 5,
-      title: "Design Keser Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 6,
-      title: "Design Banking App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-  ];
-  
-  export const contentPortfolio = [
-    {
-      id: 1,
-      title: "Content Social Media App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 2,
-      title: "Content Rampa UI Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 3,
-      title: "Content E-commerce Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 4,
-      title: "Content Relax Mobile App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 5,
-      title: "Content Keser Web Design",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    },
-    {
-      id: 6,
-      title: "Content Banking App",
-      img:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
   ];
